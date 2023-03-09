c0a30b5bbe4ac525f817381a0cec2850bd7aabea
Rafael Leite de Freitas <rlfreitas@iff.edu.br>
2023-03-09 10:14:30 -0300

    melhorando código do sistema base

---
+++ main/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm
+
+from main.models import Usuario
+from sistema_base_r2 import settings
+
+
+class UsuarioForm(ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['username', 'first_name', 'last_name', 'email', 'contato', 'data_nascimento', 'groups', 'is_active', 'is_superuser' ]
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super(UsuarioForm, self).save(commit=False)
+
+        if not user.pk:
+            user.set_password(settings.DEFAULT_PASSWORD) #Set de default password
+            user.is_staff = True
+        if commit:
+            user.save()
+        return user
