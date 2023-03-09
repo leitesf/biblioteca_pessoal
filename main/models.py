8e0329b6fb98545fcd77d9eaa04f76e0277b5976
Rafael Leite de Freitas <rlfreitas@iff.edu.br>
2023-03-09 10:12:02 -0300

    melhorando código do sistema base

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    contato = models.CharField("Contato", max_length=100)
+    data_nascimento = models.DateField("Data de Nascimento", null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+
+    def __str__(self):
+        return self.get_full_name()
+
+    def get_edit_url(self):
+        return '/admin/main/usuario/{}/change/'.format(self.id)
