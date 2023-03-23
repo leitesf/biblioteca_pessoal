18afe68ce24e5c33cf274035d5a1fb05ecdf6f71
Rafael Leite de Freitas <rlfreitas@iff.edu.br>
2023-03-23 16:43:02 -0300

    melhorando urls

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,16 @@
 from django.conf import settings
-from django.urls import path, include
-from django.views.generic import TemplateView
-from main import views
 from django.conf.urls.static import static
+from django.urls import path
+
+from main import views
 
 app_name = 'main'
 
 urlpatterns = [
-                  path('autor/<int:autor_id>/', views.show_autor),
-                  path('livro/<int:livro_id>/', views.show_livro),
-                  path('categoria/<int:categoria_id>/', views.show_categoria),
-                  path('editora/<int:editora_id>/', views.show_editora),
-                  path('idioma/<int:idioma_id>/', views.show_idioma),
-                  path('estante/<int:estante_id>/', views.show_estante),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('autor/<int:autor_id>/', views.show_autor),
+    path('livro/<int:livro_id>/', views.show_livro),
+    path('categoria/<int:categoria_id>/', views.show_categoria),
+    path('editora/<int:editora_id>/', views.show_editora),
+    path('idioma/<int:idioma_id>/', views.show_idioma),
+    path('estante/<int:estante_id>/', views.show_estante),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
